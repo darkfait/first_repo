57df2c892a635a38694830ccca73fed9d33a0765
darkfait <fait-forever@mail.ru>
2020-05-31 11:56:54 +0000

    Внес изменения в последние две задачки. Подчистил код немного

--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -27,11 +27,7 @@
 
 with open('CAM_table.txt') as cam_table:
     new_cam_list = []
-    sorted_new_cam_list = []
     sncl_1 = []
-    sncl_2 = []
-    sncl_3 = []
-    sncl_4 = []
     for line in cam_table:
         if line.count('.') != 2:
             continue
@@ -49,8 +45,7 @@ with open('CAM_table.txt') as cam_table:
     for mac_line in sorted(new_cam_list):
         if int(mac_line[0]) < 10000 and int(mac_line[0]) >= 1000:
             sncl_1.append(mac_line)
-    sorted_new_cam_list = sncl_1 + sncl_2 + sncl_3 + sncl_4
-    for line in sorted_new_cam_list:
+    for line in sncl_1:
         print('{:<10}   {:<10}  {:<10}'.format(line[0],line[1],line[2]))
             
                 
--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -15,11 +15,7 @@
 
 with open('CAM_table.txt') as cam_table:
     new_cam_list = []
-    sorted_new_cam_list = []
     sncl_1 = []
-    sncl_2 = []
-    sncl_3 = []
-    sncl_4 = []
     for line in cam_table:
         if line.count('.') != 2:
             continue
@@ -37,9 +33,8 @@ with open('CAM_table.txt') as cam_table:
     for mac_line in sorted(new_cam_list):
         if int(mac_line[0]) < 10000 and int(mac_line[0]) >= 1000:
             sncl_1.append(mac_line)
-    sorted_new_cam_list = sncl_1 + sncl_2 + sncl_3 + sncl_4
     input_vlan = input('Введите номер Vlan:   ')
-    for line in sorted_new_cam_list:
+    for line in sncl_1:
         if line[0] == input_vlan:
             print('{:<10}   {:<10}  {:<10}'.format(line[0],line[1],line[2]))
             
